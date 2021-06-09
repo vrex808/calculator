0e26af3e3e143bfc7b9fce1270dcc539f0f42baf
vrex808 <vrex808@email.com>
2021-06-08 19:38:01 -0700

    create calculator to convert gal to lit and C to F

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,38 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
-alert(add(number1, number2));
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function fToC(number1) {
+  return ((number1 - 32) * 5) / 9;
+}
+
+function galToLit(number1) {
+  return number1 / 3.78541;
+}
+
+
+//back (business logic) end stuff. 👆
+
+
+//front (user interface logic) end stuff. 👇
+
+//Here are some prompts.
+const number1 = prompt("Enter a number:");
+const number2 = prompt("Enter another number:");
+
+//specify which operation will occur.
+const result = galToLit(number1, number2);
+
+//shows the user what is up my cool gangster friend buddy pal.
+alert(result);
