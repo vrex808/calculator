cf8fadcaa871ce968f68a0e6f914ecca4eb8fe51
vrex808 <vrex808@email.com>
2021-06-14 20:47:13 -0700

    add the subtraction form and debugged

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,28 @@ function subtract(number1, number2) {
 
 function multiply(number1, number2) {
   return number1 * number2;
-};
+}
 
 function divide(number1, number2) {
   return number1 / number2;
-};
+}
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
-    event.preventDefault();
+   event.preventDefault();
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    alert(result);
+    $("#output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#sub").submit(function(event) {
+   event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output2").text(result);
   });
 });
