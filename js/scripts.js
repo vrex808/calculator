e32d61aa59bf5d641c7d64d069e0ebe905361a21
vrex808 <vrex808@email.com>
2021-06-14 19:54:33 -0700

    add event.preventDefault function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-// Business logic:
-
 function add(number1, number2) {
   return number1 + number2;
 }
@@ -10,17 +8,18 @@ function subtract(number1, number2) {
 
 function multiply(number1, number2) {
   return number1 * number2;
-}
+};
 
 function divide(number1, number2) {
   return number1 / number2;
-}
-
-// Everything below this line is user interface logic:
+};
 
 $(document).ready(function() {
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
-  const result = add(number1, number2);
-  alert(result);
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    alert(result);
+  });
 });
